5df47c1ee1d367917d84f4f08278958540671c47
user131901 <user131901@EU238.local>
2017-10-25 18:55:13 +0100

    Base project created

---
+++ soletechs/ViewModels/ServiceConfigViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace soletechs.ViewModels
+{
+    public class ServiceConfigViewModel:BaseViewModel
+    {
+        public ServiceConfigViewModel()
+        {
+        }
+
+        string companyName = string.Empty;
+        public string CompanyName
+        {
+            get { return companyName; }
+            set { SetProperty(ref companyName, value); }
+        }
+
+        string companyPassword = string.Empty;
+        public string CompanyPassword
+        {
+            get { return companyPassword; }
+            set { SetProperty(ref companyPassword, value); }
+        }
+    }
+}
---
+++ soletechs/Views/ServiceConfigurationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace soletechs.Views
+{
+    public partial class ServiceConfigurationPage : ContentPage
+    {
+        public ServiceConfigurationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
